20646488856aedfb4fcdd9ac0815b529c63d161a
Yuskab <yuskab021993@gmail.com>
2022-12-09 14:33:48 -0800

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all except e and q 
+ * Return: Always 0
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != 'e' && al != 'q')
+			putchar(al);
+	}
+	putchar('\n');
+
+	return (0);
+}
