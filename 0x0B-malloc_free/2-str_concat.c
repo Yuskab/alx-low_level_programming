3412034d2b24b1f11b27fb786067040eebdb87ef
Yuskab <yuskab021993@gmail.com>
2023-01-10 07:56:49 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -51,7 +51,8 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; *(s1 + i) != '\0'; i++)
 
 		*(dst + i) = *(s1 + i);
-	for (j = 0; *(s2 + j) != '\0'; i++)
+
+	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
 		*(dst + i) = *(s2 + j);
 		i++;
