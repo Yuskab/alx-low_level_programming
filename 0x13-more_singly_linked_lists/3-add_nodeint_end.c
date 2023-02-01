4029191689f9c390063ea2aeec360696a984c27b
Yuskab <117763067+Yuskab@users.noreply.github.com>
2023-02-02 02:26:39 +0300

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,27 +12,26 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *endnode;
-	listint_t *newnode;
+	listint_t *new, *last;
 
-	newnode = malloc(sizeof(listint_t));
-	if (!newnode)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
 
-	newnode->n = n;
-	newnode->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = newnode;
-		return (newnode);
+		*head = new;
+
 	}
-	endnode = *head;
-
-
-	while (endnode->next != NULL)
-		endnode = endnode->next;
-	endnode->next = newnode;
-
-	return (newnode);
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
 }
